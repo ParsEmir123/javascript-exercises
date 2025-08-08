c377dc661687665c48fced621aaf2c643e24a615
ParsEmir123 <wfgmemir@gmail.com>
2025-08-08 06:19:02 +0300

    solved 16 this is the last commit goodbye git

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    oldest = 0;
+    ages = people.map(person => {if(person.yearOfDeath!= undefined) return person.yearOfDeath -person.yearOfBirth; else return 2025-person.yearOfBirth})
+    
+    for (let i = 0; i < ages.length; i++) {
+        if(ages[i]>oldest) oldest = ages[i];
+        
+    }
+    return people[ages.indexOf(oldest)].name;
 };
 
 // Do not edit below this line
