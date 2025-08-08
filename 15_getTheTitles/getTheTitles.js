f293b5c7722a1c546f2f5c9c640759662cff63ec
ParsEmir123 <wfgmemir@gmail.com>
2025-08-08 06:09:33 +0300

    solved 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    titles = []
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        titles[index] = element.title;
+    }
+    return titles;
 };
 
 // Do not edit below this line
