fd81d82b57509465a77d1f2bdaf5e934ba0685cf
ParsEmir123 <wfgmemir@gmail.com>
2025-08-08 06:05:16 +0300

    13 solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let cleantext = text.replace(/[^a-zA-z0-9]/g, "");
+    length = cleantext.length;
+    cleantext = cleantext.toLowerCase();
+    for (let index = 0; index < length; index++) {
+        isokay = cleantext[index] === cleantext[length-index-1]
+        if(!isokay) return false; 
+    }
+    return true;
 };
 
 // Do not edit below this line
