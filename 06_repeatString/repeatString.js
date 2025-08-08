29c3a069192a3e3cad2642792a9040d86f6ec8a2
ParsEmir123 <wfgmemir@gmail.com>
2025-08-08 04:07:09 +0300

    solved problem 4 and 6 , 5 is just too easy

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text,num) {
+let string = "";
+for (let index = 0; index < num; index++) {
+    string += text;
+    
+}
+return string;
 };
 
 // Do not edit below this line
